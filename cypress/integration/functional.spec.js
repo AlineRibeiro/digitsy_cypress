0edec701575261d5ecaf76b610104d042f752147
Aline Ribeiro <alinear.mail@gmail.com>
2020-06-29 17:56:59 -0700

    Test log in and create product

---
+++ cypress/integration/functional.spec.js
@@ -0,0 +1,28 @@
+import 'cypress-file-upload';
+
+describe('Should test at functional level', () => {
+  before(() => {
+    cy.visit('http://localhost:3001/session/new');
+    cy.get(':nth-child(1) > .ui > input').type('aline@email.com');
+    cy.get(':nth-child(2) > .ui > input').type('supersecret');
+    cy.get('.stacked > .teal').click();
+    cy.get('.current-user').should('contain', 'Hello, Aline!')
+  });
+
+  it ('Should insert a product', () => {
+    cy.get('.right > .my-menu').click();
+    cy.get(':nth-child(1) > .ui > input').type(`Cypress test product ${Math.random()}`);
+    cy.get(':nth-child(2) > .ui > input').type('20.00');
+    cy.get('textarea').type('This is a description for my Cypress test product. It must be at least 50 chars long');
+    cy.get('input[name="thumbnail"]').attachFile('../fixtures/bootcamp_image.png');
+    cy.get('input[name="sample_media"]').attachFile({filePath: '../fixtures/bootcamp_sample.pdf', encoding: 'base64'});
+    cy.get('input[name="media"]').attachFile({filePath: '../fixtures/bootcamp_product.pdf', encoding: 'base64'});
+    cy.get(':nth-child(9) > .ui').click();
+  });
+
+  // it.only()
+
+
+});
+
+
