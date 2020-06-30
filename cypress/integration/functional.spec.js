bc50307a6290b48e26a1ca616245dcca081fb071
Aline Ribeiro <alinear.mail@gmail.com>
2020-06-30 10:32:41 -0700

    Create object for productCreate locators

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -1,24 +1,36 @@
-import 'cypress-file-upload';
-import  locators from '../support/locators';
+import "cypress-file-upload";
+import locators from "../support/locators";
 
-describe('Should test at functional level', () => {
+describe("Should test at functional level", () => {
   before(() => {
-    cy.visit('http://localhost:3001/session/new');
-    cy.get(locators.LOGIN.USER).type('aline@email.com');
-    cy.get(locators.LOGIN.PASSWORD).type('supersecret');
-    cy.get(locators.LOGIN.BUTTON).click();
-    cy.get(locators.GREETING.CURRENT_USER).should('contain', 'Hello, Aline!')
+    cy.visit("http://localhost:3001/session/new");
+    cy.get(locators.LOGIN.USER).type("aline@email.com");
+    cy.get(locators.LOGIN.PASSWORD).type("supersecret");
+    cy.get(locators.LOGIN.BTN_LOGIN).click();
+    cy.get(locators.MENU.CURRENT_USER).should("contain", "Hello, Aline!");
   });
 
-  it ('Should insert a product', () => {
-    cy.get('.right > .my-menu').click();
-    cy.get(':nth-child(1) > .ui > input').type(`Cypress test product ${Math.random()}`);
-    cy.get(':nth-child(2) > .ui > input').type('20.00');
-    cy.get('textarea').type('This is a description for my Cypress test product. It must be at least 50 chars long');
-    cy.get('input[name="thumbnail"]').attachFile('../fixtures/bootcamp_image.png');
-    cy.get('input[name="sample_media"]').attachFile({filePath: '../fixtures/bootcamp_sample.pdf', encoding: 'base64'});
-    cy.get('input[name="media"]').attachFile({filePath: '../fixtures/bootcamp_product.pdf', encoding: 'base64'});
-    cy.get(':nth-child(9) > .ui').click();
+  it("Should insert a product", () => {
+    cy.get(locators.MENU.SELL_PRODUCT).click();
+    cy.get(locators.CREATE_PRODUCTS.NAME).type(
+      `Cypress test product ${Math.random()}`
+    );
+    cy.get(locators.CREATE_PRODUCTS.PRICE).type("20.00");
+    cy.get(locators.CREATE_PRODUCTS.DESCRIPTION).type(
+      "This is a description for my Cypress test product. It must be at least 50 chars long"
+    );
+    cy.get(locators.CREATE_PRODUCTS.THUMBNAIL).attachFile(
+      "../fixtures/bootcamp_image.png"
+    );
+    cy.get(locators.CREATE_PRODUCTS.SAMPLE_MEDIA).attachFile({
+      filePath: "../fixtures/bootcamp_sample.pdf",
+      encoding: "base64",
+    });
+    cy.get(locators.CREATE_PRODUCTS.MEDIA).attachFile({
+      filePath: "../fixtures/bootcamp_product.pdf",
+      encoding: "base64",
+    });
+    cy.get(locators.CREATE_PRODUCTS.BTN_CREATE).click();
     // cy.url().should('be', 'http://localhost:3001/products'); falta um wait aqui. algo assim
   });
 });
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -1,11 +1,21 @@
 const locators = {
   LOGIN: {
-    USER: ':nth-child(1) > .ui > input',
-    PASSWORD: ':nth-child(2) > .ui > input',
-    BUTTON: '.stacked > .teal'
+    USER: ":nth-child(1) > .ui > input",
+    PASSWORD: ":nth-child(2) > .ui > input",
+    BTN_LOGIN: ".stacked > .teal",
   },
-  GREETING: {
-    CURRENT_USER: '.current-user'
+  MENU: {
+    CURRENT_USER: ".current-user",
+    SELL_PRODUCT: ".right > .my-menu",
+  },
+  CREATE_PRODUCTS: {
+    NAME: ":nth-child(1) > .ui > input",
+    PRICE: ":nth-child(2) > .ui > input",
+    DESCRIPTION: "textarea",
+    THUMBNAIL: 'input[name="thumbnail"]',
+    SAMPLE_MEDIA: 'input[name="sample_media"]',
+    MEDIA: 'input[name="media"]',
+    BTN_CREATE: ":nth-child(9) > .ui",
   }
 };
 
