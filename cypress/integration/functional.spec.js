fa63ace315e2138b1f374b069c65c2bdaa0864eb
Aline Ribeiro <alinear.mail@gmail.com>
2020-07-03 15:56:26 -0700

    Code cleanup

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -38,18 +38,15 @@ describe("Should test at functional level", () => {
 
   it.only("should get total cart price", () => {
     cy.accessBrowseProducts();
-    cy.xpath(locators.productIndex.FN_XP_SHOPPING_CART_BTN("Living the pug life")).click();
+    cy.xpath(
+      locators.productIndex.FN_XP_SHOPPING_CART_BTN("Living the pug life")
+    ).click();
     cy.accessBrowseProducts();
-    cy.xpath(locators.productIndex.FN_XP_SHOPPING_CART_BTN("React Hooks")).click();
+    cy.xpath(
+      locators.productIndex.FN_XP_SHOPPING_CART_BTN("React Hooks")
+    ).click();
     cy.checkTotalPrice("147.41");
     cy.removeCartItem();
-    cy.removeCartItem();
-    cy.checkTotalPrice("0.00");
-  });
-
-  it("should remove item from shopping cart", () => {
-    cy.accessBrowseProducts();
-    cy.get(locators.productIndex.topShoppingCart).click();
     cy.removeCartItem();
     cy.checkTotalPrice("0.00");
   });
