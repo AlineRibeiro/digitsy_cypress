eb71bba05a1f0d6cb1eb780185e3b1d82846f7d8
Aline Ribeiro <alinear.mail@gmail.com>
2020-07-09 12:11:59 -0700

    Test response status, product name and id properties

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -9,30 +9,36 @@ describe("Should test at functional level", () => {
 
   it("should insert a product", () => {
     cy.request({
-      method: 'POST',
-      url: 'http://localhost:3000/api/session',
+      method: "POST",
+      url: "http://localhost:3000/api/session",
       body: {
         email: "aline@email.com",
-        password: "supersecret"
-      }
-    }).its('body.user.id').should('not.be.empty');
-
-
+        password: "supersecret",
+      },
+    })
+      .its("body.user.id")
+      .should("not.be.empty");
     cy.request({
       method: "POST",
       url: "http://localhost:3000/api/products",
       headers: {
         "Content-Type":
-          "multipart/form-data; boundary=----WebKitFormBoundaryoh3GYY6WEwLKtV1k"
+          "multipart/form-data; boundary=----WebKitFormBoundaryoh3GYY6WEwLKtV1k",
       },
       form: true,
       body: {
-        "product[name]": "Prod11",
-        "product[price]": "80.00",
+        "product[name]": "Prod73",
+        "product[price]": "130.00",
         "product[description]":
-          "The description has to be at least 50 chars long or it wont work"
-      }
-    }).then(res => console.log(res));
+          "The description has to be at least 50 chars long or it wont work",
+      },
+    }).as("response");
+
+    cy.get("@response").then((res) => {
+      expect(res.status).to.be.equal(201);
+      expect(res.body).to.have.property('product').to.have.property('id');
+      expect(res.body).to.have.property('product').to.have.property('name', "Prod73");
+    });
   });
 
   it("should not create a product with existing name", () => {});
