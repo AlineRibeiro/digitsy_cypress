ad18fc72e5cf6ac369cc8e141ff4a3aa1c1b3416
Aline Ribeiro <alinear.mail@gmail.com>
2020-07-09 11:18:07 -0700

    Test login and create product requests separately

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -1,6 +1,5 @@
 import "cypress-file-upload";
 
-
 describe("Should test at functional level", () => {
   beforeEach(() => {
     // cy.login("aline@email.com", "supersecret", "Hello, Aline!");
@@ -17,13 +16,26 @@ describe("Should test at functional level", () => {
         password: "supersecret"
       }
     }).its('body.user.id').should('not.be.empty');
+
+
+    cy.request({
+      method: "POST",
+      url: "http://localhost:3000/api/products",
+      headers: {
+        "Content-Type":
+          "multipart/form-data; boundary=----WebKitFormBoundaryoh3GYY6WEwLKtV1k"
+      },
+      form: true,
+      body: {
+        "product[name]": "Prod11",
+        "product[price]": "80.00",
+        "product[description]":
+          "The description has to be at least 50 chars long or it wont work"
+      }
+    }).then(res => console.log(res));
   });
 
-  it("should not create a product with existing name", () => {
+  it("should not create a product with existing name", () => {});
 
-  });
-
-  it("should get total cart price", () => {
-
-  });
+  it("should get total cart price", () => {});
 });
